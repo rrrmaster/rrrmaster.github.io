20516bd279339128ee588310985350a4c69f03ed
rrrmaster <dudgus9101@gmail.com>
2022-09-24 18:43:06 +0900

    feat: seo

--- gatsby-ssr.ts
+++ gatsby-ssr.ts
@@ -1,3 +1,11 @@
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+// Adds a class name to the body element
+exports.onRenderBody = (
+  { setBodyAttributes, setHtmlAttributes },
+  pluginOptions
+) => {
   setHtmlAttributes({ lang: "ko" });
+
+  setBodyAttributes({
+    className: "my-body-class",
+  });
 };
